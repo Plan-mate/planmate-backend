cd7c460280695311927777de1abfd46994f07425
ktkdgh <dev.ktkdgh@gmail.com>
2025-08-05 19:51:18 +0900

    init: 프로젝트 초기화

---
+++ src/main/java/com/planmate/planmate_backend/PlanmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.planmate.planmate_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanmateBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planmate/planmate_backend/PlanmateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planmate.planmate_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanmateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

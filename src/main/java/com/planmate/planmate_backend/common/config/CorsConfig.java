6cc6a3ffb7ae6ff52339153973f543e2611c7918
ktkdgh <dev.ktkdgh@gmail.com>
2025-10-30 22:39:01 +0900

    fix: 프론트 도메인 연결로 인해 cors 설정 수정

--- src/main/java/com/planmate/planmate_backend/common/config/CorsConfig.java
+++ src/main/java/com/planmate/planmate_backend/common/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://planmate-frontend.vercel.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://www.planmate.shop"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH","DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
